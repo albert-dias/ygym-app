4e5b698a51ac95cbb5a673078dbd067bbde3d220
Albert Dias <albert.uft@gmail.com>
2023-11-21 22:47:15 -0300

    Update theme to navigation with native base

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,13 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import { AuthRoutes } from "./auth.routes";
+import { useTheme } from "native-base";
 
 export function Routes(){
+  const {colors} = useTheme()
+  const theme = DefaultTheme;
+  theme.colors.background = colors.gray[700]
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={theme}>
       <AuthRoutes />
     </NavigationContainer>
   )
