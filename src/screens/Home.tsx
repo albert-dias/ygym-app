7075b68b24b442a3479441a72166faeb5aec073d
Albert Dias <albert.uft@gmail.com>
2023-11-30 22:17:23 -0300

    group buttom

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,25 @@
-import { HomeHeader } from "@components/HomeHeader";
-import { Center, Text, VStack } from "native-base";
+import { Group } from '@components/Group';
+import { HomeHeader } from '@components/HomeHeader';
+import { HStack, VStack } from 'native-base';
+import { useState } from 'react';
 
-export function Home(){
-  return(
+export function Home() {
+  const [groupSelected, setGroupSelected] = useState('costas');
+  return (
     <VStack flex={1}>
       <HomeHeader />
+      <HStack>
+        <Group
+          name="costas"
+          isActive={groupSelected === 'costas'}
+          onPress={() => setGroupSelected('costas')}
+        />
+        <Group
+          name="ombro"
+          isActive={groupSelected === 'ombro'}
+          onPress={() => setGroupSelected('ombro')}
+        />
+      </HStack>
     </VStack>
-  )
+  );
 }
