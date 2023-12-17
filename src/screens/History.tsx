72a836e681212a9e63b67c5f74f4f70ab2bde42b
Albert Dias <albert.uft@gmail.com>
2023-12-16 23:09:20 -0300

    finish history screen

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,9 +1,137 @@
-import { Center, Text } from "native-base";
+import { HistoryCard } from "@components/HistoryCard";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { Heading, SectionList, Text, VStack } from "native-base";
+import { useState } from "react";
 
 export function History(){
+  const [exercises, setExercises] = useState([
+    {
+      title: "10/12/2023",
+      data:[
+        {
+          title: 'Costas',
+          name: 'Puxada frontal'
+        },
+        {
+          title: 'Costas',
+          name: 'Puxada frontal'
+        },
+        {
+          title: 'Costas',
+          name: 'Puxada frontal'
+        },
+        {
+          title: 'Costas',
+          name: 'Puxada frontal'
+        },
+        {
+          title: 'Costas',
+          name: 'Puxada frontal'
+        },
+      ]
+    },
+    {
+      title: "11/12/2023",
+      data:[
+        {
+          title: 'Costas',
+          name: 'Puxada frontal'
+        },
+        {
+          title: 'Costas',
+          name: 'Puxada frontal'
+        },
+        {
+          title: 'Costas',
+          name: 'Puxada frontal'
+        },
+        {
+          title: 'Costas',
+          name: 'Puxada frontal'
+        },
+        {
+          title: 'Costas',
+          name: 'Puxada frontal'
+        },
+      ]
+    },
+    {
+      title: "13/12/2023",
+      data:[
+        {
+          title: 'Costas',
+          name: 'Puxada frontal'
+        },
+        {
+          title: 'Costas',
+          name: 'Puxada frontal'
+        },
+        {
+          title: 'Costas',
+          name: 'Puxada frontal'
+        },
+        {
+          title: 'Costas',
+          name: 'Puxada frontal'
+        },
+        {
+          title: 'Costas',
+          name: 'Puxada frontal'
+        },
+      ]
+    },
+    {
+      title: "14/12/2023",
+      data:[
+        {
+          title: 'Costas',
+          name: 'Puxada frontal'
+        },
+        {
+          title: 'Costas',
+          name: 'Puxada frontal'
+        },
+        {
+          title: 'Costas',
+          name: 'Puxada frontal'
+        },
+        {
+          title: 'Costas',
+          name: 'Puxada frontal'
+        },
+        {
+          title: 'Costas',
+          name: 'Puxada frontal'
+        },
+      ]
+    },
+  ])
+
   return(
-    <Center flex={1}>
-      <Text>History</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title='Histórico de Exercícios'/>
+
+      <SectionList
+        sections={exercises} 
+        keyExtractor={item => item.name}
+        renderItem={({item})=> (
+          <HistoryCard />
+        )}
+        renderSectionHeader={({section}) => (
+          <Heading color='gray.200' fontSize='md' mt={10} mb={3}>{section.title}</Heading>
+        )}
+        px={8}
+        contentContainerStyle={exercises.length === 0 && {flex: 1, justifyContent: 'center'}}
+        ListEmptyComponent={() => (
+          <Text color='gray.100' textAlign='center'>
+            Não há exercícios registrados ainda.{'\n'}
+            Vamos nos exercitar hoje?
+          </Text>
+        )}
+        showsVerticalScrollIndicator={false}
+      />
+      
+      
+    </VStack>
   )
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,25 +1,51 @@
+import { ExerciseCard } from '@components/ExerciseCard';
 import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
-import { HStack, VStack } from 'native-base';
+import { FlatList, HStack, Heading, Text, VStack } from 'native-base';
 import { useState } from 'react';
 
 export function Home() {
+  const [groups, setGroups] = useState(['costas', 'Bíceps ', 'Tríceps', 'ombro']);
+  const [exercises, setExercises] = useState(['Puxada frontal', 'Remada curvada ', 'Remada unilateral', 'Supino retono', 'Rosca inversa']);
   const [groupSelected, setGroupSelected] = useState('costas');
   return (
     <VStack flex={1}>
       <HomeHeader />
-      <HStack>
-        <Group
-          name="costas"
-          isActive={groupSelected === 'costas'}
-          onPress={() => setGroupSelected('costas')}
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({item}) =>(
+          <Group
+          name={item}
+          isActive={groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()}
+          onPress={() => setGroupSelected(item)}
         />
-        <Group
-          name="ombro"
-          isActive={groupSelected === 'ombro'}
-          onPress={() => setGroupSelected('ombro')}
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{
+          px:8
+        }}
+        my={10}
+        maxH={10}
+      />
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">Exercícios</Heading>
+          <Text color="gray.200" fontSize="sm">{exercises.length}</Text>
+        </HStack>
+        <FlatList 
+          data={exercises}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <ExerciseCard />
+          )}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{
+            paddingBottom: 20
+          }}
         />
-      </HStack>
+      </VStack>
     </VStack>
   );
 }
