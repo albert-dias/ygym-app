336d28820a8852c8b0e1f8a325dc65d503d99fd2
Albert Dias <albert.uft@gmail.com>
2024-01-04 09:50:15 -0300

    validation form

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -6,12 +6,26 @@ import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 import { useNavigation } from '@react-navigation/native';
 
+import { useForm, Controller } from 'react-hook-form';
+
+type FormDataProps ={
+  name: string,
+  email: string,
+  password: string,
+  password_confirm: string
+}
+
 export function SignUp() {
+  const { control, handleSubmit, register, formState:{ errors} } = useForm<FormDataProps>();
 
   const navigation = useNavigation();
 
   function handleGoBack(){
     navigation.goBack()
+  }
+
+  function handdleSignUp(data: FormDataProps){
+
   }
 
   return (
@@ -37,17 +51,92 @@ export function SignUp() {
           <Heading color="green.100" fontSize="xl" mb={6} fontFamily="heading">
             Crie sua conta
           </Heading>
-
-          <Input placeholder="Nome" />
-
-          <Input
-            placeholder="E-mail"
-            keyboardType="email-address"
-            autoCapitalize="none"
+          <Controller
+            control={control}
+            name="name"
+            rules={{
+              required: 'Informe um nome'
+            }}
+            render={({field: {onChange, value, ref}}) => (
+              <Input 
+                placeholder="Nome" 
+                onChangeText={onChange}
+                value={value}   
+                errorMessage={errors.name?.message}
+              /> 
+            )}
           />
-          <Input placeholder="Senha" secureTextEntry />
 
-          <Button title="Criar e acessar" />
+
+          <Controller
+            control={control}
+            name="email"
+            rules={{
+              required: 'informe o email',
+              pattern: {
+                value:/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: 'E-mail inválido'
+              }
+            }}
+            render={({field: {onChange, value, ref}}) => (
+              <Input
+                placeholder="E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.email?.message}
+                // returnKeyType='next'
+              />
+            )}
+          />
+          
+          <Controller
+            control={control}
+            name="password"
+            rules={{
+              required: 'Digite sua senha',
+              minLength: 8
+            }}
+            render={({field: {onChange, value, ref}}) => (
+              <Input 
+                placeholder="Senha" 
+                secureTextEntry 
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.password?.message}
+                // returnKeyType='next'
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="password_confirm"
+            rules={{
+              required: 'Confirme sua senha'
+            }}
+            render={({field: {onChange, value, ref}}) => (
+              <Input 
+                placeholder="Confirme a senha" 
+                secureTextEntry 
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.password_confirm?.message}
+                ref={(e) => {
+                  ref(e);
+                  register(e)
+                }}
+                onSubmitEditing={handleSubmit(handdleSignUp)}
+                returnKeyType='send'
+              />
+            )}
+          />
+
+          <Button 
+            onPress={handleSubmit(handdleSignUp)}
+            title="Criar e acessar" 
+          />
         </Center>
         <Center mt={24}>
           <Button title="Voltar para o login" variant="outline" onPress={handleGoBack} />
@@ -56,3 +145,10 @@ export function SignUp() {
     </ScrollView>
   );
 }
+
+
+// .wrapper {
+//   display: grid;
+//   grid-template-columns: repeat(auto-fit, minmax(min(11.25rem, 100%), 1fr));
+//   grid-gap: 1rem;
+// }
